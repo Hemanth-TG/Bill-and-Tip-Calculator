a8f0c96ed1ab042e923f1613b6e27db71fcbaca0
HemanthTG <109616729+Hemanth-TG@users.noreply.github.com>
2025-07-20 23:29:23 +0530

    Add files via upload
    Added JS file for script

---
+++ bill-and-tip.js
@@ -0,0 +1,17 @@
+const calculate= () => {
+                        const bill=Number(document.getElementById("bill").value.trim());
+                        const tipPercent=Number(document.getElementById("tip").value.trim());
+                        const persons=Number(document.getElementById("people").value.trim());
+                        if(!bill || bill<=0 || tipPercent<0 || persons<=0 ) {
+                            document.getElementById("result").innerText="Please enter valid poisitve numbers!";
+                            return;
+                        }
+                        const tip=bill*(tipPercent/100);
+                        const total=bill+tip;
+                        const perPerson=total/persons;
+                        
+                        // document.getElementById("result").innerText=`Total: ₹${total.toFixed(2)} | Each Pays: ₹${perPerson.toFixed(2)}`;
+                        document.getElementById("result").innerText =
+  `Bill: ₹${bill.toFixed(2)} | Tip: ₹${tip.toFixed(2)}\nTotal: ₹${total.toFixed(2)} | Each Pays: ₹${perPerson.toFixed(2)}`;
+
+                    }
